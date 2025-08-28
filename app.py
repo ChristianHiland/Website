adb49bfb945d6a36d917e2515081f1e99cd8008c
Lee Lunbin <hilandchristian112@gmail.com>
2025-08-28 17:43:12 -0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='src', static_url_path='/src')
 
 # Default 'Home' Page
 @app.route('/')
